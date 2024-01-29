713a2a3829493f8608e594e135e0f4f52c98b3f6
Afolabi Temilade <afolstee@gmail.com>
2024-01-29 13:55:54 +0100

    media queries

---
+++ order/order.js
@@ -0,0 +1,22 @@
+// document.querySelectorAll(".delivery-option-input")
+// .forEach((input) => {
+//     input.addEventListener("click", () => {
+//         if(input.setAttribute === ("checked")) {
+//             input.removeAttribute("checked");
+//         } else {
+//             input.setAttribute("checked")
+//         }
+//     });
+// });
+
+function click() {
+    const input = document.querySelector(".delivery-option-input");
+    if() {
+        input.setAttribute("checked");
+    } else {
+        input.removeAttribute("checked");
+    }
+}
+
+console.log(input);
+
--- script.js
+++ script.js
@@ -12,19 +12,20 @@ products.forEach((product) => {
     <div class="product-name">${product.name}</div>
     <div class="rating-stars"><img src="images/ratings/rating-${product.rating.stars * 10}.png" alt="" class="rating-img"><div class="rating-number">${product.rating.count}</div></div>
     <div class="price">$${(product.priceCents / 100).toFixed(2)}</div>
-    <select class="cart-quatity js-quantity-selector-${product.id}">
-        <option selected value="1">1</option>
-        <option value="2">2</option>
-        <option value="2">3</option>
-        <option value="2">4</option>
-        <option value="2">5</option>
-        <option value="2">6</option>
-        <option value="2">7</option>
-        <option value="2">8</option>
-        <option value="2">9</option>
-        <option value="2">10</option>
-    </select>
-   <div class="addtocart" ><button class="js-add-to-cart" data-product-id="${product.id}">
+    <select class="js-quantity-selector-${product.id} cart-quatity">
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+        </select>
+   <div class="addtocart" >
+   <button class="js-add-to-cart" data-product-id="${product.id}">
    Add to Cart</button></div>
 </div>`;
 });
@@ -32,6 +33,7 @@ document.querySelector(".product-container").innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     button.addEventListener("click", () => {
+       
         const productId = button.dataset.
         productId;
     
@@ -46,6 +48,7 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         const quantitySelector = document.querySelector(
           `.js-quantity-selector-${productId}`);
         const quantity = Number(quantitySelector.value);
+        quantitySelector.value = '1';
         
         console.log(quantity);
         if(matchingItem) {
