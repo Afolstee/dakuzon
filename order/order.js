1f2a874d39ad30b4ed41c15d6e5109f0b915bf3d
Afolabi Temilade <afolstee@gmail.com>
2024-01-30 22:06:44 +0100

    cart styling

--- order/order.js
+++ order/order.js
@@ -1,22 +1,80 @@
-// document.querySelectorAll(".delivery-option-input")
-// .forEach((input) => {
-//     input.addEventListener("click", () => {
-//         if(input.setAttribute === ("checked")) {
-//             input.removeAttribute("checked");
-//         } else {
-//             input.setAttribute("checked")
-//         }
-//     });
-// });
+import { cart } from '../data/cart.js';
+import { products } from '../data/products.js';
 
-function click() {
-    const input = document.querySelector(".delivery-option-input");
-    if() {
-        input.setAttribute("checked");
-    } else {
-        input.removeAttribute("checked");
-    }
-}
+let cartSummaryHTML = '';
 
-console.log(input);
+cart.forEach((cartItem) => {
+    const productId = cartItem.productId;
+    let matchingProduct;
 
+    products.forEach((product) => {
+        if (product.id === productId) {
+            matchingProduct = product;
+        }
+    });
+
+    cartSummaryHTML += `
+        
+    <div class="product-info">
+    <img src="${matchingProduct.image}" class="product-img" />
+    <div class="order-info">
+      <div class="product-title">
+        ${matchingProduct.name}
+      </div>
+      <div class="price">$${(matchingProduct.priceCents / 100).toFixed(2)}</div>
+      <div class="quantity">
+        <span>Quantity: <span class="quantity-value"> ${cartItem.quantity} </span></span
+        >
+      </div>
+      <div class="buttons">
+        <button class="update">Update</button
+        ><button class="delete">Delete</button>
+     </div>
+    </div>
+    <div class="delivery-options">
+      <div class="choose-delivery">Choose a delivery option:</div>
+      <div class="delivery-container">
+        <div class="delivery-option">
+          <div class="input">
+            <input
+              type="radio"
+              checked
+              class="delivery-option-input"
+              onclick="click()"
+            />
+          </div>
+          <div id="delivery-price">
+            <div id="delivery-date">Tuesday, June 21</div>
+            <div class="delivery-price">Free shipping</div>
+          </div>
+        </div>
+        <div class="delivery-option">
+          <div class="input">
+            <input
+              type="radio"
+              onclick="click()"
+              class="delivery-option-input"
+            />
+          </div>
+          <div id="delivery-price">
+            <div id="delivery-date">Tuesday, June 21</div>
+            <div class="delivery-price">Free shipping</div>
+          </div>
+        </div>
+        <div class="delivery-option">
+          <div class="input">
+            <input type="radio" class="delivery-option-input" />
+          </div>
+          <div id="delivery-price">
+            <div id="delivery-date">Tuesday, June 21</div>
+            <div class="delivery-price">Free shipping</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    `
+});
+
+document.querySelector('.js-order-summary')
+.innerHTML = cartSummaryHTML
--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-
-let productsHTML = '';
+import { cart, addToCart } from "./data/cart.js";
+import { products } from "./data/products.js";
+let productsHTML = "";
 
 
 products.forEach((product) => {
-    productsHTML += 
-    `<div class="product">
+  productsHTML += `<div class="product">
     <div class="product-image">
     <img src="${product.image}" alt="" class="product-img">
     </div>
     
     <div class="product-name">${product.name}</div>
-    <div class="rating-stars"><img src="images/ratings/rating-${product.rating.stars * 10}.png" alt="" class="rating-img"><div class="rating-number">${product.rating.count}</div></div>
+    <div class="rating-stars"><img src="images/ratings/rating-${
+      product.rating.stars * 10
+    }.png" alt="" class="rating-img"><div class="rating-number">${
+    product.rating.count
+  }</div></div>
     <div class="price">$${(product.priceCents / 100).toFixed(2)}</div>
     <select class="js-quantity-selector-${product.id} cart-quatity">
             <option selected value="1">1</option>
@@ -31,48 +35,23 @@ products.forEach((product) => {
 });
 document.querySelector(".product-container").innerHTML = productsHTML;
 
-document.querySelectorAll(".js-add-to-cart").forEach((button) => {
-    button.addEventListener("click", () => {
-       
-        const productId = button.dataset.
-        productId;
-    
-        let matchingItem;
-    
-        cart.forEach((item) => {
-          if (productId === item.productId) {
-            matchingItem = item;
-          }
-        });
-    
-        const quantitySelector = document.querySelector(
-          `.js-quantity-selector-${productId}`);
-        const quantity = Number(quantitySelector.value);
-        quantitySelector.value = '1';
-        
-        console.log(quantity);
-        if(matchingItem) {
-          matchingItem.quantity += quantity;
-        } else {
-          cart.push({
-          productId: productId,
-          quantity: quantity
-        });
-        }
-    
-        let cartQuantity = 0;
-    
-        cart.forEach((item) => {
-          cartQuantity += item.quantity;
-        });
-    
-        document.querySelector('.js-cart-quantity')
-         .innerHTML = cartQuantity;
-    
-        
-        
-        
-        console.log(cart);
-        
-      });
+ function updateCartQuantity() {
+   let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
     });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+    
+    
+}
+
+document.querySelectorAll(".js-add-to-cart")
+.forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    addToCart(productId);
+    updateCartQuantity();
+  });
+});
